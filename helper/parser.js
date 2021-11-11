cc0ab452704ca749125c923a90a88f5991281e65
RyanPrabowo <ryan.prabowo@omniretail.co>
2021-11-11 15:57:19 +0700

    styling pokemon type

--- helper/parser.js
+++ helper/parser.js
@@ -23,6 +23,39 @@ export const typeParser = (type) => {
     case 'normal':
       color = 'gray-500';
       return color;
+    case 'electric':
+      color = 'yellow-400';
+      return color;
+    case 'ground':
+      color = 'yellow-700';
+      return color;
+    case 'fairy':
+      color = 'pink-500';
+      return color;
+    case 'fighting':
+      color = 'red-800';
+      return color;
+    case 'psychic':
+      color = 'pink-700';
+      return color;
+    case 'rock':
+      color = 'yellow-800';
+      return color;
+    case 'steel':
+      color = 'gray-600';
+      return color;
+    case 'ice':
+      color = 'blue-300';
+      return color;
+    case 'ghost':
+      color = 'purple-900';
+      return color;
+    case 'dragon':
+      color = 'indigo-700';
+      return color;
+    case 'dark':
+      color = 'yellow-900';
+      return color;
     default:
       color = 'current';
       return color;
